ea38c439084d0acd8722a94c46aa29971d72370c
locemarn <marcelon27@gmail.com>
2018-02-23 12:15:08 -0300

    One post details

--- resources/views/web/post.blade.php
+++ resources/views/web/post.blade.php
@@ -5,22 +5,31 @@
   <div class="row">
 
     <div class="col-md-8 col-md-offset-2">
-      <h1>Lista de artigos</h1>
-      @foreach($posts as $post)
+      <h1>{{ $post->name }}</h1>
+      
       <div class="panel panel-default">
         <div class="panel-heading">
-          {{ $post->name }}
+          Categoria
+        <a href="#">{{ $post->category->name }}</a>
         </div>
         <div class="panel-body">
           @if($post->file)
           <img src="{{ $post->file }}" class="img-responsive">
           @endif
           {{ $post->excerpt }}
-          <a href="{{ route('post', $post->slug) }}" class="pull-right">Ler mais</a>
+          <hr>
+          {{!! $post->body !!}}
+          <hr>
+          Etiquetas
+          @foreach($post->tags as $tag)
+          <a href="#">
+            {{ $tag->name }}
+          </a>
+          @endforeach
         </div>
       </div>
-      @endforeach
-      {{ $posts->render() }}
+      
+      
     </div>
   </div>
 </div>
