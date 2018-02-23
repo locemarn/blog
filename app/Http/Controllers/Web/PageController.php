f4d2c6251be28829f44485047220d21a0ae6ff37
locemarn <marcelon27@gmail.com>
2018-02-23 00:34:20 -0300

    exhib posts - pagination

--- app/Http/Controllers/Web/PageController.php
+++ app/Http/Controllers/Web/PageController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers\Web;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Post;
+
 class PageController extends Controller
 {
     public function blog(){
-        return view('web.posts');
+        $posts = Post::orderBy('id', 'DESC')->where('status', 'PUBLISHED')->paginate(3);
+        return view('web.posts', compact('posts'));
     }
 }
--- resources/views/web/posts.blade.php
+++ resources/views/web/posts.blade.php
@@ -4,6 +4,21 @@
 <div class="container">
   <div class="col-md-8 col-md-offset-2">
     <h1>Lista de artigos</h1>
+    @foreach($posts as $post)
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        {{ $post->name }}
+      </div>
+      <div class="panel-body">
+        @if($post->file)
+          <img src="{{ $post->file }}" class="img-responsive">
+        @endif
+        {{ $post->excerpt }}
+        <a href="#" class="pull-right">Ler mais</a>
+      </div>
+    </div>
+    @endforeach
+    {{ $posts->render() }}
   </div>
 </div>
 @endsection
