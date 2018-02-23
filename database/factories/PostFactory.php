40b1c92ae4d3e3ad03dc1c06248cb9d243a6a644
locemarn <marcelon27@gmail.com>
2018-02-22 23:41:05 -0300

    little adjustments

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -12,6 +12,6 @@ $factory->define(App\Post::class, function (Faker $faker) {
         'excerpt'     => $faker->text(200),
         'body'        => $faker->text(500),
         'file'        => $faker->imageUrl($width = 1200, $height = 400),
-        'status'      => $faker->randomEllement(['DRAFT', 'PUBLISHED'])
+        'status'      => $faker->randomElement(['DRAFT', 'PUBLISHED'])
     ];
 });
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,7 +11,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Category::class, 29)->create();
+        factory(App\User::class, 29)->create();
 
         App\User::create([
             'name'      => 'Marcelo Nogueira',
