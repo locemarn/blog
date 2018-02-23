bb9ced3bf907407a0ee65482aed74be41a5308b0
locemarn <marcelon27@gmail.com>
2018-02-22 23:58:30 -0300

    Relations

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = [
+        'user_id', 'category_id', 'name', 'slug', 'excerpt', 'body', 'status', 'file'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::tag);
+    }
+
+    public function category(){
+        return $this->belongsTo(Catedory::tag);
+    }
+
     public function tags(){
         return $this->belongsToMany(Tag::class);
     }
