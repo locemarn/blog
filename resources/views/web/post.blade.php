75ac93394a1348c401255e1148e4831b35e3f81d
locemarn <marcelon27@gmail.com>
2018-02-24 14:04:59 -0300

    little adjustments

--- resources/views/web/post.blade.php
+++ resources/views/web/post.blade.php
@@ -10,17 +10,19 @@
       <div class="panel panel-default">
         <div class="panel-heading">
           Categoria
-        <a href="{{ route('category', $post->category->slug) }}">{{ $post->category->name }}</a>
+        <a href="{{ route('category', $post->category->slug) }}"> {{ $post->category->name }}</a>
         </div>
         <div class="panel-body">
           @if($post->file)
           <img src="{{ $post->file }}" class="img-fluid">
           @endif
+          <strong>Excerpt:</strong>
           {{ $post->excerpt }}
           <hr>
-          {{!! $post->body !!}}
+          <strong>Body:</strong>
+          {!! $post->body !!}
           <hr>
-          Etiquetas
+          <strong>Tags</strong>
           @foreach($post->tags as $tag)
           <a href="{{ route('tag', $tag->slug) }}">
             {{ $tag->name }}
